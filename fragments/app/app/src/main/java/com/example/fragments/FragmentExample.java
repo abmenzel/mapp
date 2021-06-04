5b09be50c4f7d61ac5e0b54edd8ef9b3394bcc46
Alexander <alexander@menzel.dk>
2021-06-04 15:59:51 +0200

    comments for fragments

--- fragments/app/app/src/main/java/com/example/fragments/FragmentExample.java
+++ fragments/app/app/src/main/java/com/example/fragments/FragmentExample.java
@@ -1,17 +1,36 @@
 package com.example.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.app.Fragment;
+import android.widget.TextView;
 
 public class FragmentExample extends Fragment {
 
+    TextView text;
+
+    // 1. When a fragment view is created, we must use an inflater to create views for the given xml file.
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment, container, false);
+        text = view.findViewById(R.id.frag_title);
         return view;
+    }
+
+    // 2. When the activity hosting our fragment is created, we pass some information from the activity onto the fragment.
+    //    In this case I passed some text and a color string.
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            text.setText(bundle.getString("text"));
+            text.setBackgroundColor(Color.parseColor(bundle.getString("color")));
+        }
     }
 
 
--- fragments/app/app/src/main/java/com/example/fragments/MainActivity.java
+++ fragments/app/app/src/main/java/com/example/fragments/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
 
@@ -13,9 +15,6 @@ public class MainActivity extends AppCompatActivity {
     Button frag1_button;
     Button frag2_button;
 
-    boolean frag1_attached = false;
-    boolean frag2_attached = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,36 +23,51 @@ public class MainActivity extends AppCompatActivity {
         frag1_button = findViewById(R.id.frag1);
         frag2_button = findViewById(R.id.frag2);
 
+        // 3. We use a fragment manager to keep track of all the fragments that exist in our activity.
         FragmentManager fm = getFragmentManager();
 
+        // 4. We set up two buttons to add/remove our fragment 1 and 2, using the samle fragment xml file.
+        //    We pass different information to each fragment.
         frag1_button.setOnClickListener(v -> {
-            if(frag1_attached){
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.remove(fm.findFragmentById(R.id.frag1_container));
-                ft.commit();
-                frag1_attached = false;
+            FragmentTransaction ft = fm.beginTransaction();
+            Fragment existingFrag = fm.findFragmentById(R.id.frag1_container);
+
+            // 5. We check whether a fragment already exists, if not we create it.
+            if(existingFrag == null){
+                Fragment frag = new FragmentExample();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("text", "Fragment1");
+                bundle.putString("color", "#6b3ea3");
+                frag.setArguments(bundle);
+
+                ft.add(R.id.frag1_container, frag, null);
             }else{
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.frag1_container, new FragmentExample(), null);
-                ft.commit();
-                frag1_attached = true;
+                ft.remove(existingFrag);
             }
+            ft.commit();
         });
+
+        // 6. Fragments are useful for modularity, and reusability.
+        // For example, here we are re-using the fragments in the landscape layout.
 
         frag2_button.setOnClickListener(v -> {
-            if(frag2_attached){
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.remove(fm.findFragmentById(R.id.frag2_container));
-                ft.commit();
-                frag2_attached = false;
+            FragmentTransaction ft = fm.beginTransaction();
+            Fragment existingFrag = fm.findFragmentById(R.id.frag2_container);
+
+            if(existingFrag == null){
+                Fragment frag = new FragmentExample();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("text", "Fragment2");
+                bundle.putString("color", "#ac11fa");
+
+                frag.setArguments(bundle);
+                ft.add(R.id.frag2_container, frag, null);
             }else{
-                FragmentTransaction ft = fm.beginTransaction();
-                ft.add(R.id.frag2_container, new FragmentExample(), null);
-                ft.commit();
-                frag2_attached = true;
+                ft.remove(existingFrag);
             }
+            ft.commit();
         });
-
-
     }
 }
